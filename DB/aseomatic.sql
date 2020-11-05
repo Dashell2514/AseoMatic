1335ea4a002e60f09d1da8c919b676ecc298edb4
David <Davidhernandezjuajinoy@gmail.com>
2020-11-05 14:19:47 -0500

    Ajustar el query de la base de datos

--- DB/aseomatic.sql
+++ DB/aseomatic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2020 a las 19:51:46
+-- Tiempo de generación: 05-11-2020 a las 20:19:07
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.4
 
