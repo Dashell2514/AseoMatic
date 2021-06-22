3357b60013c2d969302cd4f8951581a5cffd9c04
David Juajinoy <davidhernandezjuajinoy@gmail.com>
2021-06-22 17:59:44 -0500

    D

--- Code/config/Config.php
+++ Code/config/Config.php
@@ -2,18 +2,18 @@
 
 class ConfigurationClient{
     //Database
-    const DB_HOST = $_ENV['DB_HOST'];
-    const DB_DATABASE = $_ENV["DB_DATABASE"];
-    const DB_USER = $_ENV["DB_USER"];
-    const DB_PASSWORD = $_ENV["DB_PASSWORD"];
-    const DB_PORT = $_ENV["DB_PORT"];
-    const DB_CHARSET = $_ENV['DB_CHARSET'];
+    const DB_HOST = 'localhost';
+    const DB_DATABASE = 'databaseaseomatic';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '';
+    const DB_CHARSET = 'charset:utf8';
+    const DB_PORT = 3306;
     
     //Mailtrap
     
-    const MAIL_HOST = $_ENV["MAIL_HOST"]; 
-    const MAIL_PORT = $_ENV["MAIL_PORT"]; 
-    const MAIL_USERNAME  = $_ENV['MAIL_USERNAME'];           
-    const MAIL_PASSWORD = $_ENV['MAIL_PASSWORD']; 
+    const MAIL_HOST = 'smtp.mailtrap.io'; 
+    const MAIL_PORT = 2525; 
+    const MAIL_USERNAME   = '028de6b4dea01b';           
+    const MAIL_PASSWORD = 'ab1fceab36503b'; 
 
 }
